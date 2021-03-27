c02e850a4265b44a6235a64a6da1cc4219468585
abenezer-dengene <abenezeret@dengene.com>
2021-03-27 22:06:13 +0300

    Regular auto-commit at 22:06:13 on 27/03/2021

--- src/main/java/com/abenezer/itom/Service/Authentication/Authentication.java
+++ src/main/java/com/abenezer/itom/Service/Authentication/Authentication.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.abenezer.itom.Service.Authentication;
 
 import com.abenezer.itom.Service.systemUserRecords.SystemUserRegistryDatabase;
@@ -18,6 +14,8 @@ import java.util.Optional;
 /**
  * @author AbenezerEsheteTilahu
  */
+
+
 @Service
 public class Authentication {
 
